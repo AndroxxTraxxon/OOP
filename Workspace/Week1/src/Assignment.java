c381022b2bad32d7d66ac8e4f2ec103067e15b9a
David Culbreth <david.culbreth.256@gmail.com>
2015-01-26 10:35:30 -0600

    Fixed Assignment file.

--- Workspace/Week1/src/Assignment.java
+++ Workspace/Week1/src/Assignment.java
@@ -5,11 +5,8 @@ public class Assignment {
 	public static void main(String[] args) {
 		int rad;
 		double area, diam;
-<<<<<<< HEAD
 		DecimalFormat dm = new DecimalFormat();
-=======
 		
->>>>>>> origin/master
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Enter the radius of the circle.");
 		
@@ -18,19 +15,13 @@ public class Assignment {
 		
 		area = circArea(rad);
 		diam = circDiam(rad);
-<<<<<<< HEAD
+
 		dm.setMaximumFractionDigits(4);
 		
 		
 		System.out.println("The circle's radius is " + rad + ", the area is " + dm.format(area));
 		System.out.println("The diameter is "+ dm.format(diam) +".");
 		
-=======
-		
-		System.out.println("The circle's radius is " + rad + ", the area is " + area);
-		System.out.println("The diameter is "+ diam +".");
-
->>>>>>> origin/master
 		return;
 		
 	}
@@ -44,8 +35,5 @@ public class Assignment {
 		
 		return 2 * rad;
 	}
-<<<<<<< HEAD
-	
-=======
->>>>>>> origin/master
+
 }
