bcf12693731516da32f3c78b0fb67c4470cb901f
David Culbreth <dculbreth@mail.stmarytx.edu>
2015-01-30 10:54:47 -0600

    Week 2 Assignments

---
+++ Workspace/Week2/src/MilesPerGallon.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class MilesPerGallon {
+
+	public static void main(String[] args) {
+		double miles, gallons, mpg;
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Enter the miles driven: ");
+		miles = scan.nextDouble();
+		System.out.print("Enter the gallons of fuel used: ");
+		gallons = scan.nextDouble();
+		mpg = miles/gallons;
+		System.out.println("The miles-per-gallon is " + mpg);
+		
+		scan.close();
+		System.exit(0);
+	}
+	
+	
+
+}
