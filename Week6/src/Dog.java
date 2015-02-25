eafc0990ce4df0dcc0424a76507b78de9571ab54
David Culbreth <dculbreth@mail.stmarytx.edu>
2015-02-25 11:00:08 -0600

    Dog and Dog Test In-Class examples

---
+++ Week6/src/Dog.java
@@ -0,0 +1,14 @@
+
+public class Dog {
+	public int size;
+	public String breed;
+	public String name;
+	
+	
+	public void bark(){
+		System.out.println("Dog barks");
+		
+	}
+	
+
+}
---
+++ Week6/src/DogTest.java
@@ -0,0 +1,12 @@
+
+public class DogTest {
+	
+	public static void main(String[] args){
+		Dog d = new Dog();
+		d.size = 40;
+		d.breed = "Bulldog";
+		d.bark();
+		d.bark();
+		
+	}
+}
