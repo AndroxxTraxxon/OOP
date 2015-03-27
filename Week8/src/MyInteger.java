11d88a88e68eda719ff8a41981c4de28e7fb8761
David Culbreth <dculbreth@mail.stmarytx.edu>
2015-03-27 11:11:46 -0500

    Week 9 Push

--- Week8/src/MyInteger.java
+++ Week8/src/MyInteger.java
@@ -1,12 +1,46 @@
 
 public class MyInteger {
 	
+	private int data;
+	
+	public MyInteger(int value){
+		data = value;
+	}
+	
+	public MyInteger(){
+		data = 0;
+	}
+	
+	public int getData() {
+		return data;
+	}
+
+	public void setData(int data) {
+		this.data = data;
+	}
+
 	public static boolean isEven(int num){
 		return (num%2==0);	
 	}
 	
+	public static boolean isEven(MyInteger num){
+		return (num.getData()%2==0);	
+	}
+	
+	public boolean isEven(){
+		return (data%2==0);	
+	}
+	
 	public static boolean isOdd(int num){
 		return (num%2==1);
+	}
+	
+	public static boolean isOdd(MyInteger num){
+		return (num.getData()%2==1);
+	}
+	
+	public boolean isOdd(){
+		return (data%2==1);
 	}
 	
 	public static boolean isPrime(int num){
@@ -15,5 +49,44 @@ public class MyInteger {
 			if (num%j==0)return false;
 		}
 		return true;
+	}
+	
+	public boolean isPrime(){
+		if (data < 2) return false;
+		for(long j = 2; j<(int)(Math.sqrt(data)+1); j++){
+			if (data%j==0)return false;
+		}
+		return true;
+	}
+	
+	public static boolean isPrime(MyInteger number){
+		int num = number.getData();
+		if (num < 2) return false;
+		for(long j = 2; j<(int)(Math.sqrt(num)+1); j++){
+			if (num%j==0)return false;
+		}
+		return true;
+	}
+	
+	public boolean equals(int a){
+		return (data==a);
+	}
+	
+	public boolean equals(MyInteger a){
+		return(data==a.getData());
+	}
+	
+	public static int parseInt(String a){
+		int num;
+		java.util.Scanner scan= new java.util.Scanner(a);
+		num = scan.nextInt();
+		scan.close();
+		return num;
+		
+	}
+	
+	public String toString(){
+		return "MyInteger [data = "+data+"]" +((isEven())?"[EVEN]":"[ODD]")+((isPrime())?"[PRIME]":"");
+		
 	}
 }
--- Week8/src/TestInteger.java
+++ Week8/src/TestInteger.java
@@ -6,11 +6,26 @@ public class TestInteger {
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Please enter an integer:");
 		int input = scan.nextInt();
+		scan.close();
 		
-		System.out.println(" isEven: ["+MyInteger.isEven(input)+"]");
-		System.out.println("  isOdd: ["+MyInteger.isOdd(input)+"]");
+		
+		System.out.println("isEven: ["+MyInteger.isEven(input)+"]");
+		System.out.println("isOdd: ["+MyInteger.isOdd(input)+"]");
 		System.out.println("isPrime: ["+MyInteger.isPrime(input)+"]");
 		
-		scan.close();
+		MyInteger myInt = new MyInteger(1233);
+		MyInteger myPInt = new MyInteger(MyInteger.parseInt("457"));
+		
+		System.out.println("myInt:s"+myInt);
+		System.out.println("myInt.isEven: ["+myInt.isEven()+"]");
+		System.out.println("myInt.isOdd: ["+myInt.isOdd()+"]");
+		System.out.println("myInt.isPrime: ["+myInt.isPrime()+"]");
+		System.out.println("myPInt:" + myPInt);
+		System.out.println("myPInt.isEven: ["+MyInteger.isEven(myPInt)+"]");
+		System.out.println("myPInt.isOdd: ["+MyInteger.isOdd(myPInt)+"]");
+		System.out.println("myPInt.isPrime: ["+MyInteger.isPrime(myPInt)+"]");
+		System.out.println("[myInt = myPInt]:" + myInt.equals(myPInt));
+		System.out.println("[myInt = input]:" + myInt.equals(input));
+		
 	}
 }
