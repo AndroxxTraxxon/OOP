8240dbd56644430e87b30289a7e96850d65f7d8c
David Culbreth <dculbreth@mail.stmarytx.edu>
2015-04-19 20:14:20 -0500

    Fixed improper output in distinct.java

--- Week10/src/Distinct.java
+++ Week10/src/Distinct.java
@@ -6,10 +6,10 @@ public class Distinct {
 		Scanner scan = new Scanner(System.in);
 		int[] input = new int[10];
 		int count=0;
-		for(int i = 0; i < input.length; i++){
+		for(int i = 0; i < input.length; i++){// for a given number of inputs
 			System.out.print("Enter value " + (i+1) + ": ");
 			int in = scan.nextInt();
-			if(!contains(input, in)){
+			if(!contains(input, in)){//if the input is not already in the array, add it to the array.
 				input[count] = in;
 				count++;
 			}	
@@ -18,14 +18,12 @@ public class Distinct {
 		System.out.println("The mutually distinct numbers are as follows:");
 		for(int i = 0; i < count; i++){
 			System.out.print(input[i]+" ");
-		}for(int i = 0; i < count; i++){
-			System.out.print(input[i]+" ");
 		}
 		
 		scan.close();
 	}
 	
-	static boolean contains(int[]arr, int num){
+	static boolean contains(int[]arr, int num){//returns if an int array contains a given value
 		for(int i = 0; i < arr.length; i++){
 			if(arr[i]==num)return true;
 		}
